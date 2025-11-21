198f3a1bb7f5e2e39266a7a105c8b353da9a5223
Adilson Perinei <zumiki@gmail.com>
2025-11-21 15:04:54 -0500

    update script

--- bootstrap-ubuntu-pi.sh
+++ bootstrap-ubuntu-pi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "raspberry PI GPIO and Arduino-iot-cloud"
 
+echo "Updating system and installing dependencies..."
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y install python3-full
@@ -8,19 +9,26 @@ sudo apt -y install liblgpio-dev
 sudo apt -y install swig
 sudo apt -y install git
 
+
 #git repo clone
+echo "Cloning repository..."
 git config --global user.name "app"
 git config --global user.email "app@localhost.com"
 git clone https://github.com/zumiki-creator/doorbell-raspberry-pi.git /home/iot/app
 
 #ubuntu only
+echo "Installing Ubuntu specific dependencies..."
 sudo apt-get install crossbuild-essential-arm64
 sudo apt-get install python3-dev
-# ---------------
 
+#setup python environment
+echo "Setting up Python environment..."
 cd /home/iot/app
 python3 -m venv venv
 source venv/bin/activate
+
+# upgrade pip and install required python packages
+echo "Installing Python packages..."
 
 pip install --upgrade pip setuptools wheel
 
@@ -30,12 +38,13 @@ pip install RPi-lgpio
 pip install arduino-iot-cloud
 
 # create app service   
+echo "Setting up application service..."
 chmod +x /home/iot/app/run-app.sh
 sudo cp /home/iot/app/bootstrap/doorbell.service /etc/systemd/system/doorbell.service
 sudo systemctl enable doorbell.service
 sudo systemctl start doorbell.service 
 
 # install ssh server
-sudo apt -y install openssh-server
-sudo systemctl enable ssh
-sudo systemctl start ssh
+# sudo apt -y install openssh-server
+# sudo systemctl enable ssh
+# sudo systemctl start ssh
