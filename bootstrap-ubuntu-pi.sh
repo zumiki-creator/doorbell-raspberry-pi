ff49d341226590ffe5b3254790d6b3fb13fed749
Adilson Perinei <zumiki@gmail.com>
2025-11-21 11:18:34 -0500

    ready for first test

--- bootstrap-ubuntu-pi.sh
+++ bootstrap-ubuntu-pi.sh
@@ -6,14 +6,19 @@ sudo apt -y upgrade
 sudo apt -y install python3-full
 sudo apt -y install liblgpio-dev
 sudo apt -y install swig
+sudo apt -y install git
+
+#git repo clone
+git config --global user.name "app"
+git config --global user.email "app@localhost.com"
+git clone https://github.com/zumiki-creator/doorbell-raspberry-pi.git /home/iot/app
 
 #ubuntu only
 sudo apt-get install crossbuild-essential-arm64
 sudo apt-get install python3-dev
 # ---------------
 
-mkdir ~/app
-cd ~/app
+cd /home/iot/app
 python3 -m venv venv
 source venv/bin/activate
 
@@ -23,6 +28,12 @@ pip install gpiozero
 pip install RPi-GPIO
 pip install RPi-lgpio
 pip install arduino-iot-cloud
+
+# create app service   
+chmod +x /home/iot/app/run-app.sh
+sudo cp /home/iot/app/bootstrap/doorbell.service /etc/systemd/system/doorbell.service
+sudo systemctl enable doorbell.service
+sudo systemctl start doorbell.service 
 
 # install ssh server
 sudo apt -y install openssh-server
